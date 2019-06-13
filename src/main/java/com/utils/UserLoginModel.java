14440a78f8970c2702237ba09c2c655798a1e3b8
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-06-13 04:31:56 +0000

    fix issues

--- src/main/java/com/utils/UserLoginModel.java
+++ src/main/java/com/utils/UserLoginModel.java
@@ -5,7 +5,7 @@ public class UserLoginModel {
     private String login;
     private String password;
 
-    String getLogin() {
+    public String getLogin() {
         return login;
     }
 
@@ -15,7 +15,7 @@ public class UserLoginModel {
 //    }
 // --Commented out by Inspection STOP (13/06/2019 04:23)
 
-    String getPassword() {
+    public String getPassword() {
         return password;
     }
 
